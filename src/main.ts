eb5cddfad504fd07f7ebc4227043a81e68abd05d
Ganeshkumar208 <ganeshkumarakula17@gmail.com>
2025-01-07 15:14:36 +0530

    swaggger adde d to the backend

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 5566);
-// }
-// bootstrap();
-
-
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Enable CORS
   app.enableCors({
-    origin: '*', // Update to the frontend domain in production
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
 
-  const port = process.env.PORT ?? 5566;
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Application')
+    .setDescription('API documentation for the NestJS Application')
+    .setVersion('1.0')
+    .addTag('NestJS API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 5566;
   await app.listen(port);
-  Logger.log(`Server is running on http://localhost:${port}`);
+
+  Logger.log(`🚀 Server is running on http://localhost:${port}`);
+  Logger.log(`📚 Swagger UI is available at http://localhost:${port}/api`);
 }
+
 bootstrap();
